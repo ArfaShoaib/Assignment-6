a6c5d3298230e591373d9698b2c62f741fbdaa5b
ArfaShoaib <arfa67264@gmail.com>
2024-11-24 17:39:50 +0500

    m2

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <div className="container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div className=" flex flex-col md:flex-row items-center justify-center gap-8 mb-5 mt-12 md:mt-0 py-12 md:py-24">
+      <div className=" flex flex-col md:flex-row items-center justify-center gap-8 mb-5 mt-12 md:mt-0 py-12 md:py-24 xl:ml-40">
         {/* Left Text Content */}
         <div className="w-full space-y-6 xs:text-left xs:self-start xl:mt-44 md:mt-40">
           <h1 className="text-[24px] xs:text-[30px] sm:text-[40px] md:text-[48px]  md:leading-[50px] md:tracking-tight lg:text-[56px] font-Roboto font-bold leading-[32px] xs:leading-[40px] sm:leading-[48px] lg:leading-[57px]">
@@ -26,9 +26,9 @@ const Hero = () => {
         </div>
 
         {/* Right Image Content */}
-        <div className="w-full flex justify-center xs:w-full sm:w-3/4 md:w-1/2 md:justify-end md:-ml-20">
-          <div className="relative w-full xs:max-w-[428px] sm:max-w-[350px] md:w-[350px] h-[250px] xs:h-[390px] sm:h-[400px] md:h-[500px] lg:h-[600px] lg:w-[640px] mb-5 ">
-            <Image src="/Image.png" alt="Learning illustration" fill priority className="object-contain lg:object-cover" />
+        <div className="w-full flex justify-center xs:w-full sm:w-3/4 md:w-1/2 md:justify-end md:-ml-20 lg:w-[600px]">
+          <div className="relative w-full xs:max-w-[428px] sm:max-w-[350px] md:w-[350px] h-[250px] xs:h-[390px] sm:h-[400px] md:h-[500px] lg:h-[600px] lg:w-[640px] mb-5  ">
+            <Image src="/Image.png" alt="Learning illustration" fill priority />
           </div>
         </div>
       </div>
